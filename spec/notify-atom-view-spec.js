85f43d6cc0af5fca163ee0ebe00e8a9048860c76
Filip Haglund <filip@happypie.com>
2017-03-17 22:26:20 +0100

    Initial functionality

---
+++ spec/notify-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NotifyAtomView from '../lib/notify-atom-view';
+
+describe('NotifyAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
